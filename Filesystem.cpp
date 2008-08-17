fa44a0afa4d21c0507f7c6d6e8c7f89f586103f9
Bartosz Taudul <wolf.pld@gmail.com>
2008-08-17 23:15:01 +0200

    Add checking if file exists.

--- Filesystem.cpp
+++ Filesystem.cpp
@@ -16,7 +16,6 @@ bool Filesystem::ChangeDirectory( const std::string& cd )
 {
 	PathVector reqPath = SplitPath( cd );
 	PathVector path;
-
 	if( cd[0] == '/' )
 	{
 		path = SplitPath( "/" );
@@ -26,41 +25,45 @@ bool Filesystem::ChangeDirectory( const std::string& cd )
 		path = SplitPath( m_path );
 	}
 
-	for( PathVector::const_iterator it = reqPath.begin(); it != reqPath.end(); ++it )
+	if( !TryChangePath( reqPath, path ) )
 	{
-		if( *it == "." )
-		{
-			continue;
-		}
-		else if( *it == ".." )
-		{
-			if( path.size() == 0 )
-			{
-				// Can't go below the root directory.
-				// Or maybe it should be possible as no-op?
-				return false;
-			}
-			else
-			{
-				path.pop_back();
-			}
-		}
-		else
-		{
-			if( DirectoryExists( MakePath( path ) + "/" + *it ) )
-			{
-				path.push_back( *it );
-			}
-			else
-			{
-				return false;
-			}
-		}
+		return false;
 	}
 
 	m_path = MakePath( path );
 
 	return true;
+}
+
+bool Filesystem::FileExists( const std::string& file )
+{
+	PathVector reqPath = SplitPath( file );
+
+	// Remove filename from path
+	std::string fname = reqPath.back();
+	reqPath.pop_back();
+
+	PathVector path;
+	if( file[0] == '/' )
+	{
+		path = SplitPath( "/" );
+	}
+	else
+	{
+		path = SplitPath( m_path );
+	}
+
+	if( !TryChangePath( reqPath, path ) )
+	{
+		return false;
+	}
+
+	return CheckFileExists( MakePath( path ) + "/" + fname );
+}
+
+FILE* Filesystem::FileOpen( const std::string& file, Mode mode )
+{
+	return NULL;
 }
 
 std::string Filesystem::MakePath( const PathVector& pv )
@@ -93,5 +96,55 @@ bool Filesystem::DirectoryExists( const std::string& dir )
 		return false;
 	}
 
-	return (s.st_mode & S_IFMT) == S_IFDIR;
+	return S_ISDIR( s.st_mode );
+}
+
+bool Filesystem::TryChangePath( const PathVector& reqPath, PathVector& path )
+{
+	for( PathVector::const_iterator it = reqPath.begin(); it != reqPath.end(); ++it )
+	{
+		if( *it == "." )
+		{
+			continue;
+		}
+		else if( *it == ".." )
+		{
+			if( path.size() == 0 )
+			{
+				// Can't go below the root directory.
+				// Or maybe it should be possible as no-op?
+				return false;
+			}
+			else
+			{
+				path.pop_back();
+			}
+		}
+		else
+		{
+			if( DirectoryExists( MakePath( path ) + "/" + *it ) )
+			{
+				path.push_back( *it );
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool Filesystem::CheckFileExists( const std::string& file )
+{
+	std::string path = m_root + file;
+	struct stat s;
+
+	if( stat( path.c_str(), &s ) == -1 )
+	{
+		return false;
+	}
+
+	return S_ISREG( s.st_mode );
 }
--- Filesystem.hpp
+++ Filesystem.hpp
@@ -1,6 +1,7 @@
 #ifndef __DFTPD__FILESYSTEM_HPP__
 #define __DFTPD__FILESYSTEM_HPP__
 
+#include <stdio.h>
 #include <string>
 #include <boost/shared_ptr.hpp>
 #include "String.hpp"
@@ -8,15 +9,26 @@
 class Filesystem
 {
 public:
+	enum Mode
+	{
+		M_READ,
+		M_WRITE
+	};
+
 	Filesystem( const std::string& root );
 	~Filesystem();
 
 	const std::string& GetPath() const { return m_path; }
 	bool ChangeDirectory( const std::string& cd );
 
+	bool FileExists( const std::string& file );
+	FILE* FileOpen( const std::string& file, Mode mode );
+
 private:
 	std::string MakePath( const PathVector& pv );
 	bool DirectoryExists( const std::string& dir );
+	bool TryChangePath( const PathVector& reqPath, PathVector& path );
+	bool CheckFileExists( const std::string& file );
 
 	std::string m_root;
 	std::string m_path;
