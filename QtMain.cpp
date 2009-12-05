d0141f5939abe740546c0a4b87b646486a490254
Bartosz Taudul <wolf.pld@gmail.com>
2009-12-05 01:50:28 +0100

    Log server messages on screen.

--- QtMain.cpp
+++ QtMain.cpp
@@ -1,19 +1,56 @@
 #include <QtGui>
+#include "AuthNone.hpp"
+#include "AuthToken.hpp"
+#include "Log.hpp"
+#include "Server.hpp"
+#include "ServerPtr.hpp"
+
+Log* g_log = NULL;
+
+class QtApp : public Log
+{
+public:
+	QtApp( int argc, char** argv )
+		: m_app( new QApplication( argc, argv ) )
+	{
+		g_log = this;
+	}
+
+	~QtApp()
+	{
+		delete m_app;
+	}
+
+	int Run()
+	{
+		QWidget window;
+		window.show();
+		window.setWindowTitle( "Dump FTP server" );
+
+		m_logbox = new QTextEdit();
+		m_logbox->setReadOnly( true );
+
+		QVBoxLayout* layout = new QVBoxLayout();
+		layout->addWidget( m_logbox );
+		window.setLayout( layout );
+
+		ServerPtr server( Server::Create( AuthPtr( new AuthToken ) ) );
+
+		return m_app->exec();
+	}
+
+	void Print( const std::string& text )
+	{
+		m_logbox->append( QString( text.c_str() ) );
+	}
+
+private:
+	QApplication* m_app;
+	QTextEdit* m_logbox;
+};
 
 int main( int argc, char** argv )
 {
-	QApplication app( argc, argv );
-
-	QWidget window;
-	window.show();
-	window.setWindowTitle( "Dump FTP server" );
-
-	QTextEdit* logBox = new QTextEdit();
-	logBox->setReadOnly( true );
-
-	QVBoxLayout* layout = new QVBoxLayout();
-	layout->addWidget( logBox );
-	window.setLayout( layout );
-
-	return app.exec();
+	QtApp app( argc, argv );
+	return app.Run();
 }
